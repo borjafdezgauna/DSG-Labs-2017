06ce4adf87bd30f6a63b23b7e33cb87e7d321229
borjafdezgauna <borja.fernandez@ehu.eus>
2017-09-20 14:27:35 +0200

    Removed the solution code

--- TextGame/GameLogic.cpp
+++ TextGame/GameLogic.cpp
@@ -19,33 +19,5 @@ GameLogic::~GameLogic()
 
 bool GameLogic::processInput()
 {
-	char c;
 
-	if (!_kbhit())
-		return false;
-
-	c = _getch();
-
-	switch (c)
-	{
-	case 'd':
-	case 'D':
-		m_player.moveRight();
-		break;
-	case 'w':
-	case 'W':
-		m_player.moveUp();
-		break;
-	case 's':
-	case 'S':
-		m_player.moveDown();
-		break;
-	case 'a':
-	case 'A':
-		m_player.moveLeft();
-		break;
-	case 27: // ASCII code for the escape character
-		return true;
-	}
-	return false;
 }
--- TextGame/Player.cpp
+++ TextGame/Player.cpp
@@ -6,10 +6,7 @@
 Player::Player(World& world, int posX, int posY, char c)
 	:m_world(world)
 {
-	m_posX = std::max((int)0, std::min(world.getSizeX(), posX));
-	m_posY = std::max((int)0, std::min(world.getSizeY(), posY));
 
-	m_world.set(m_posX, m_posY, c);
 }
 
 
@@ -19,32 +16,22 @@ Player::~Player()
 
 char Player::move(int destX, int destY)
 {
-	m_world.clamp(destX, destY);
 
-	char steppedOver= m_world.move(m_posX, m_posY, destX, destY);
-	m_posX = destX;
-	m_posY = destY;
-
-	return steppedOver;
 }
 
 char Player::moveUp()
 {
-	int destX = m_posX, destY = m_posY-1;
-	return move(destX, destY);
+
 }
 char Player::moveDown()
 {
-	int destX = m_posX, destY = m_posY + 1;
-	return move(destX, destY);
+
 }
 char Player::moveRight()
 {
-	int destX = m_posX+1, destY = m_posY;
-	return move(destX, destY);
+
 }
 char Player::moveLeft()
 {
-	int destX = m_posX-1, destY = m_posY;
-	return move(destX, destY);
+
 }
