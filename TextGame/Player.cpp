3888f77b7e99b7cb099060d3495a000ce233d936
Ebrain <Ebrainlg5@hotmail.com>
2017-09-27 15:45:35 +0200

    Player finished.

--- TextGame/Player.cpp
+++ TextGame/Player.cpp
@@ -6,6 +6,11 @@
 Player::Player(World& world, int posX, int posY, char c)
 	:m_world(world)
 {
+	m_posX = posX;
+	m_posY = posY;
+
+	m_world.set(posX, posY, c);
+
 
 }
 
@@ -16,22 +21,35 @@ Player::~Player()
 
 char Player::move(int destX, int destY)
 {
+	this->m_posX = destX;
+	this->m_posY = destY;
 
 }
 
 char Player::moveUp()
 {
-
+	if(m_posY > 0){
+	this->move(m_posX,m_posY-1);
+	}
 }
 char Player::moveDown()
 {
+	if (m_posY < m_world.getSizeY()) {
+		this->move(m_posX, m_posY + 1);
+	}
 
 }
 char Player::moveRight()
 {
+	if (m_posX < m_world.getSizeX()) {
+		this->move(m_posX+1, m_posY);
+	}
 
 }
 char Player::moveLeft()
 {
+	if (m_posX > 0) {
+		this->move(m_posX-1, m_posY);
+	}
 
 }
