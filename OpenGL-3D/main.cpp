4365060498f71a82883aba26a03dd97af419be31
Borja Fernández Gauna <bortxkudai@hotmail.com>
2017-10-24 20:54:06 +0200

    Added project OpenGL-3D

---
+++ OpenGL-3D/main.cpp
@@ -0,0 +1,47 @@
+#include "stdafx.h"
+#include "InputHandler.h"
+#include "Renderer.h"
+#include "Sphere.h"
+#include "../3rd-party/freeglut3/include/GL/freeglut.h"
+#include "Camera.h"
+
+
+int main(int argc, char** argv)
+{
+	Renderer renderer;
+	InputHandler inputHandler(renderer);
+	
+	renderer.initialize(argc, argv);
+	inputHandler.initialize();
+
+
+	//init objects
+	Sphere sphere1;
+	sphere1.setColor(255, 0, 0);
+	renderer.addObject(&sphere1);
+	sphere1.setPosition(0, 0, -5);
+	Sphere sphere2;
+	sphere2.setColor(0, 255, 0);
+	renderer.addObject(&sphere2);
+	Camera camera;
+	camera.setPosition(0, 0, 5);
+	renderer.addCamera(&camera);
+
+
+	while (1)
+	{
+		//UPDATE////////////////////
+		////////////////////////////
+		//process queued events
+		glutMainLoopEvent();
+
+
+		//RENDER////////////////////
+		////////////////////////////
+		glutPostRedisplay();
+		glutSwapBuffers();
+	}
+   return 0;
+   
+
+}
